960410b1340eebb5e250cb5fe981436cc98dffd8
jeremij94 <jeremij94@gmail.com>
2022-09-13 09:58:36 -0500

    meeting Pancho

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Meeting Pancho!
+Console.WriteLine("AY! Dios, mio! You scared me! Whew. Sorry about that. I didn't see you there. I don't see humans often. Maybe, we can be friends! Como se llama, or what is your name?");
+var amigoName = Console.ReadLine();
+
+Console.WriteLine($"Ah! So your name is {amigoName}. Encantado de conocerte! Me llamo, C# Bot Issue7779311, but you can just call me Pancho. I hear so much about humans. Your systems like to consume other organisms and call them food, right? What is your favorite food?");
+var favFood = Console.ReadLine();
+
+Console.WriteLine($"Hmmm...{favFood}. I wish I could try it. My system does not require that I eat like you do. My creator told me that his abuelitas' tamales son fantastico! O, how I wish to be human, to taste all of the comidas like you can!");
+Console.WriteLine("My research tells me humans have many interesting looking activities, too! I think you call them sports. What is your favorite human sport?");
+var favSport = Console.ReadLine();
+
+Console.WriteLine($"I would like to try {favSport}. My programs have been analyzing baseball lately. Maybe, one day we can play. Anyway, I must go recharge for another software update in a few minutes. Hasta pronto!");
+Console.ReadLine();
+
+//Stay tuned for more...
